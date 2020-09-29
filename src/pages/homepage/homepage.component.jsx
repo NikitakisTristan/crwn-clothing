a3d5dc6878543568a29d7e973f58f7075181f8ab
NikitakisTristan <tristan.nikitakis@gmail.com>
2020-09-29 18:20:17 -0400

    Updated shop data to be pulled in through a reducer

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-import Directory from '../../components/directory/directory.component'
+import Directory from "../../components/directory/directory.component";
 
-import './homepage.styles.scss'
+import { HomePageContainer } from "./homepage.styles";
 
 const Homepage = () => (
-    <div className ="homepage">
-        <Directory/>
-    </div>
-)
+  <HomePageContainer>
+    <Directory />
+  </HomePageContainer>
+);
 
-export default Homepage
+export default Homepage;
